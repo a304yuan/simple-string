04d5bc811b8c09ea19ff13a6f5cb7ea5cd468e46
a304yuan <a304yuanyuan@gmail.com>
2019-02-11 16:40:34 +0800

    init commit

---
+++ cstring.c
@@ -0,0 +1,123 @@
+#include "cstring.h"
+
+string * string_new(const char * s) {
+    string * str = malloc(sizeof(string));
+    size_t len = strlen(s);
+    void * dest = NULL;
+    if (len >= STRING_INLINE_LEN) {
+        str->data.ref = (dest = malloc(len));
+    }
+    else {
+        dest = str->data.raw;
+    }
+    memcpy(dest, s, len);
+    *(char*)(dest+len) = '\0';
+    str->len = len;
+    return str;
+}
+
+string * string_new_s(const char * s, size_t len) {
+    string * str = malloc(sizeof(string));
+    void * dest = NULL;
+    if (len >= STRING_INLINE_LEN) {
+        str->data.ref = (dest = malloc(len));
+    }
+    else {
+        dest = str->data.raw;
+    }
+    memcpy(dest, s, len);
+    *(char*)(dest+len) = '\0';
+    str->len = len;
+    return str;
+}
+
+void string_init(string * str, const char * s) {
+    size_t len = strlen(s);
+    void * dest = NULL;
+    if (len >= STRING_INLINE_LEN) {
+        str->data.ref = (dest = malloc(len));
+    }
+    else {
+        dest = str->data.raw;
+    }
+    memcpy(dest, s, len);
+    *(char*)(dest+len) = '\0';
+    str->len = len;
+}
+
+void string_init_s(string * str, const char * s, size_t len) {
+    void * dest = NULL;
+    if (len >= STRING_INLINE_LEN) {
+        str->data.ref = (dest = malloc(len));
+    }
+    else {
+        dest = str->data.raw;
+    }
+    memcpy(dest, s, len);
+    *(char*)(dest+len) = '\0';
+    str->len = len;
+}
+
+void string_free(string * str) {
+    if (str->len >= STRING_INLINE_LEN) {
+        free(str->data.ref);
+    }
+    free(str);
+}
+
+char * string_get_raw(string * str) {
+    if (str->len >= STRING_INLINE_LEN) {
+        return str->data.ref;
+    }
+    else {
+        return str->data.raw + 0;
+    }
+}
+
+void string_append(string * str, const char * s) {
+    size_t new_len = strlen(s) + str->len;
+    if (new_len < STRING_INLINE_LEN) {
+        strcpy(str->data.raw + str->len, s);
+        str->data.raw[new_len] = '\0';
+    }
+    else {
+        if (str->len >= STRING_INLINE_LEN) {
+            void * ref = realloc(str->data.ref, new_len * 2);
+            strcpy(ref + str->len, s);
+            *(char*)(ref + new_len) = '\0';
+            str->data.ref = ref;
+        }
+        else {
+            void * ref = malloc(new_len * 2);
+            memcpy(ref, str->data.raw, str->len);
+            strcpy(ref + str->len, s);
+            *(char*)(ref + new_len) = '\0';
+            str->data.ref = ref;
+        }
+    }
+    str->len = new_len;
+}
+
+void string_append_s(string * str, const char * s, size_t len) {
+    size_t new_len = len + str->len;
+    if (new_len < STRING_INLINE_LEN) {
+        strncpy(str->data.raw + str->len, s, len);
+        str->data.raw[new_len] = '\0';
+    }
+    else {
+        if (str->len >= STRING_INLINE_LEN) {
+            void * ref = realloc(str->data.ref, new_len * 2);
+            strncpy(ref + str->len, s, len);
+            *(char*)(ref + new_len) = '\0';
+            str->data.ref = ref;
+        }
+        else {
+            void * ref = malloc(new_len * 2);
+            memcpy(ref, str->data.raw, str->len);
+            strncpy(ref + str->len, s, len);
+            *(char*)(ref + new_len) = '\0';
+            str->data.ref = ref;
+        }
+    }
+    str->len = new_len;
+}
---
+++ cstring.h
@@ -0,0 +1,32 @@
+#ifndef CSTRING_H
+#define CSTRING_H
+
+#include <string.h>
+#include <stdlib.h>
+
+#define STRING_INLINE_LEN 16
+
+typedef struct string string;
+
+struct string {
+    size_t len;
+    union {
+        void * ref;
+        char raw[STRING_INLINE_LEN];
+    } data;
+};
+
+extern string * string_new(const char * s);
+extern string * string_new_s(const char * s, size_t len);
+extern void string_init(string * str, const char * s);
+extern void string_init_s(string * str, const char * s, size_t len);
+extern void string_free(string * str);
+extern char * string_get_raw(string * str);
+extern void string_append(string * str, const char * s);
+extern void string_append_s(string * str, const char * s, size_t len);
+
+static inline size_t string_len(const string * str) {
+    return str->len;
+}
+
+#endif /* end of include guard: CSTRING_H */
