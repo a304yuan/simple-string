af7690e0fbb9b8abe7990296e73505f104edfa64
a304yuan <a304yuanyuan@gmail.com>
2019-03-27 15:00:33 +0800

    change to immutable impl

--- cstring.c
+++ cstring.c
@@ -1,137 +1,42 @@
 #include "cstring.h"
 
 string * string_new(const char * s) {
-    string * str = malloc(sizeof(string));
     size_t len = strlen(s);
-    void * dest = NULL;
-    if (len >= STRING_INLINE_LEN) {
-        str->data.ref = (dest = malloc(len + 1));
-        str->cap = len;
-    }
-    else {
-        dest = str->data.raw;
-        str->cap = STRING_INLINE_LEN - 1;
-    }
-    memcpy(dest, s, len);
-    *(char*)(dest+len) = '\0';
+    string * str = malloc(sizeof(string) + len + 1);
+    strcpy(str->raw, s);
+    str->raw[len] = '\0';
     str->len = len;
-    return str;
-}
-
-string * string_new_blank() {
-    string * str = malloc(sizeof(string));
-    str->data.raw[0] = '\0';
-    str->len = 0;
-    str->cap = STRING_INLINE_LEN - 1;
     return str;
 }
 
 string * string_new_s(const char * s, size_t len) {
-    string * str = malloc(sizeof(string));
-    void * dest = NULL;
-    if (len >= STRING_INLINE_LEN) {
-        str->data.ref = (dest = malloc(len + 1));
-        str->cap = len;
-    }
-    else {
-        dest = str->data.raw;
-        str->cap = STRING_INLINE_LEN - 1;
-    }
-    memcpy(dest, s, len);
-    *(char*)(dest+len) = '\0';
+    string * str = malloc(sizeof(string) + len + 1);
+    strncpy(str->raw, s, len);
+    str->raw[len] = '\0';
     str->len = len;
     return str;
 }
-/*
-void string_init(string * str, const char * s) {
-    size_t len = strlen(s);
-    void * dest = NULL;
-    if (len >= STRING_INLINE_LEN) {
-        str->data.ref = (dest = malloc(len));
-    }
-    else {
-        dest = str->data.raw;
-    }
-    memcpy(dest, s, len);
-    *(char*)(dest+len) = '\0';
-    str->len = len;
+
+string * string_append(const string * str, const char * s) {
+    size_t len = strlen(s) + str->len;
+    string * newstr = malloc(sizeof(string) + len + 1);
+    strcpy(newstr->raw, str->raw);
+    strcpy(newstr->raw + str->len, s);
+    newstr->raw[len] = '\0';
+    newstr->len = len;
+    return newstr;
 }
 
-void string_init_s(string * str, const char * s, size_t len) {
-    void * dest = NULL;
-    if (len >= STRING_INLINE_LEN) {
-        str->data.ref = (dest = malloc(len));
-    }
-    else {
-        dest = str->data.raw;
-    }
-    memcpy(dest, s, len);
-    *(char*)(dest+len) = '\0';
-    str->len = len;
-}
-*/
-void string_free(string * str) {
-    if (str->len >= STRING_INLINE_LEN) {
-        free(str->data.ref);
-    }
-    free(str);
+string * string_append_s(const string * str, const char * s, size_t len) {
+    size_t nlen = strlen(s) + str->len;
+    string * newstr = malloc(sizeof(string) + nlen + 1);
+    strcpy(newstr->raw, str->raw);
+    strncpy(newstr->raw + str->len, s, len);
+    newstr->raw[nlen] = '\0';
+    newstr->len = nlen;
+    return newstr;
 }
 
-char * string_get_raw(string * str) {
-    if (str->len >= STRING_INLINE_LEN) {
-        return str->data.ref;
-    }
-    else {
-        return str->data.raw;
-    }
-}
-
-void string_append(string * str, const char * s) {
-    size_t new_len = strlen(s) + str->len;
-    if (new_len < STRING_INLINE_LEN) {
-        strcpy(str->data.raw + str->len, s);
-        str->data.raw[new_len] = '\0';
-    }
-    else {
-        if (str->len >= STRING_INLINE_LEN) {
-            void * ref = str->len > str->cap ? realloc(str->data.ref, new_len * 2 + 1) : str->data.ref;
-            strcpy(ref + str->len, s);
-            *(char*)(ref + new_len) = '\0';
-            str->data.ref = ref;
-        }
-        else {
-            void * ref = malloc(new_len * 2 + 1);
-            memcpy(ref, str->data.raw, str->len);
-            strcpy(ref + str->len, s);
-            *(char*)(ref + new_len) = '\0';
-            str->data.ref = ref;
-        }
-        str->cap = new_len * 2;
-    }
-    str->len = new_len;
-}
-
-void string_append_s(string * str, const char * s, size_t len) {
-    size_t new_len = len + str->len;
-    if (new_len < STRING_INLINE_LEN) {
-        strncpy(str->data.raw + str->len, s, len);
-        str->data.raw[new_len] = '\0';
-    }
-    else {
-        if (str->len >= STRING_INLINE_LEN) {
-            void * ref = str->len > str->cap ? realloc(str->data.ref, new_len * 2 + 1) : str->data.ref;
-            strncpy(ref + str->len, s, len);
-            *(char*)(ref + new_len) = '\0';
-            str->data.ref = ref;
-        }
-        else {
-            void * ref = malloc(new_len * 2 + 1);
-            memcpy(ref, str->data.raw, str->len);
-            strncpy(ref + str->len, s, len);
-            *(char*)(ref + new_len) = '\0';
-            str->data.ref = ref;
-        }
-        str->cap = new_len * 2;
-    }
-    str->len = new_len;
+string * string_concat(const string * str1, const string * str2) {
+    return string_append(str1, str2->raw);
 }
--- cstring.h
+++ cstring.h
@@ -4,29 +4,24 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define STRING_INLINE_LEN 16
-
 typedef struct string string;
 
 struct string {
     size_t len;
-    size_t cap;
-    union {
-        void * ref;
-        char raw[STRING_INLINE_LEN];
-    } data;
+    char raw[];
 };
 
 extern string * string_new(const char * s);
-extern string * string_new_blank();
 extern string * string_new_s(const char * s, size_t len);
-//extern void string_init(string * str, const char * s);
-//extern void string_init_s(string * str, const char * s, size_t len);
-extern void string_free(string * str);
-extern char * string_get_raw(string * str);
-extern void string_append(string * str, const char * s);
-extern void string_append_s(string * str, const char * s, size_t len);
-
+static inline void string_free(string * str) {
+    free(str);
+}
+static inline char * string_get_raw(string * str) {
+    return str->raw;
+}
+extern string * string_append(const string * str, const char * s);
+extern string * string_append_s(const string * str, const char * s, size_t len);
+extern string * string_concat(const string * str1, const string * str2);
 static inline size_t string_len(const string * str) {
     return str->len;
 }
